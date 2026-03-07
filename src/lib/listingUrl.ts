08f31cc349259fdfd08d9affab7d68cc0063ac9f
Isaac Mtsiriza <230100836@luanar.ac.mw>
2026-03-07 23:22:32 +0200

    Implement URL listing parameter management functions
    Added functions to manage 'listing' parameter in URL.

--- src/lib/listingUrl.ts
+++ src/lib/listingUrl.ts
@@ -1,1 +1,22 @@
+const getListingIdFromUrl = () => {
+  const params = new URLSearchParams(window.location.search);
+  return params.get("listing");
+};
 
+const buildListingShareUrl = (id: string | number) => {
+  const url = new URL(window.location.href);
+  url.searchParams.set("listing", String(id));
+  return url.toString();
+};
+
+const syncListingParamInUrl = (id: string | number) => {
+  const url = new URL(window.location.href);
+  url.searchParams.set("listing", String(id));
+  window.history.replaceState({}, "", url.toString());
+};
+
+const clearListingParamFromUrl = () => {
+  const url = new URL(window.location.href);
+  url.searchParams.delete("listing");
+  window.history.replaceState({}, "", url.toString());
+};
