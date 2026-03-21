31d732cce0bb67c99c78829e9b1a4dc5ed35119b
Isaac Mtsiriza <230100836@luanar.ac.mw>
2026-03-21 04:45:17 +0200

    Consolidate beauty listing item configs

--- src/listingSchemas/beauty/index.ts
+++ src/listingSchemas/beauty/index.ts
@@ -1,3 +1,21 @@
-import type { ListingItemConfig } from "../core";
+export * from "./shared";
+export * from "./skincare";
+export * from "./haircare";
+export * from "./makeup";
+export * from "./fragrance";
+export * from "./services";
 
-export const BEAUTY_LISTING_ITEM_CONFIGS: ListingItemConfig[] = [];
+import type { ListingItemConfig } from "../core";
+import { SKINCARE_LISTING_ITEM_CONFIGS } from "./skincare";
+import { HAIRCARE_LISTING_ITEM_CONFIGS } from "./haircare";
+import { MAKEUP_LISTING_ITEM_CONFIGS } from "./makeup";
+import { FRAGRANCE_LISTING_ITEM_CONFIGS } from "./fragrance";
+import { BEAUTY_SERVICES_LISTING_ITEM_CONFIGS } from "./services";
+
+export const BEAUTY_LISTING_ITEM_CONFIGS: ListingItemConfig[] = [
+  ...SKINCARE_LISTING_ITEM_CONFIGS,
+  ...HAIRCARE_LISTING_ITEM_CONFIGS,
+  ...MAKEUP_LISTING_ITEM_CONFIGS,
+  ...FRAGRANCE_LISTING_ITEM_CONFIGS,
+  ...BEAUTY_SERVICES_LISTING_ITEM_CONFIGS,
+];
