34f76de4c00bfd5633af1fa254c549336d9802ef
Isaac Mtsiriza <230100836@luanar.ac.mw>
2026-03-21 02:52:49 +0200

    Refactor fashion listing item configs aggregation

--- src/listingSchemas/fashion/index.ts
+++ src/listingSchemas/fashion/index.ts
@@ -1,3 +1,17 @@
-import type { ListingItemConfig } from "../core";
+export * from "./shared";
+export * from "./clothes";
+export * from "./shoes";
+export * from "./bags";
+export * from "./accessories";
 
-export const FASHION_LISTING_ITEM_CONFIGS: ListingItemConfig[] = [];
+import { CLOTHES_LISTING_ITEM_CONFIGS } from "./clothes";
+import { SHOES_LISTING_ITEM_CONFIGS } from "./shoes";
+import { BAGS_LISTING_ITEM_CONFIGS } from "./bags";
+import { ACCESSORIES_LISTING_ITEM_CONFIGS } from "./accessories";
+
+export const FASHION_LISTING_ITEM_CONFIGS = [
+  ...CLOTHES_LISTING_ITEM_CONFIGS,
+  ...SHOES_LISTING_ITEM_CONFIGS,
+  ...BAGS_LISTING_ITEM_CONFIGS,
+  ...ACCESSORIES_LISTING_ITEM_CONFIGS,
+];
