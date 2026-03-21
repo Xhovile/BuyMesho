dab51043e5b20fcc02439291bfc6e38f8c3f1810
Isaac Mtsiriza <230100836@luanar.ac.mw>
2026-03-21 11:47:44 +0200

    Aggregate academic listing item configs from modules

--- src/listingSchemas/academic/index.ts
+++ src/listingSchemas/academic/index.ts
@@ -1,3 +1,18 @@
-import type { ListingItemConfig } from "../core";
+export * from "./shared";
+export * from "./tutoring";
+export * from "./materials";
+export * from "./documents";
+export * from "./design";
 
-export const ACADEMIC_LISTING_ITEM_CONFIGS: ListingItemConfig[] = [];
+import type { ListingItemConfig } from "../core";
+import { TUTORING_LISTING_ITEM_CONFIGS } from "./tutoring";
+import { MATERIALS_LISTING_ITEM_CONFIGS } from "./materials";
+import { DOCUMENTS_LISTING_ITEM_CONFIGS } from "./documents";
+import { DESIGN_LISTING_ITEM_CONFIGS } from "./design";
+
+export const ACADEMIC_LISTING_ITEM_CONFIGS: ListingItemConfig[] = [
+  ...TUTORING_LISTING_ITEM_CONFIGS,
+  ...MATERIALS_LISTING_ITEM_CONFIGS,
+  ...DOCUMENTS_LISTING_ITEM_CONFIGS,
+  ...DESIGN_LISTING_ITEM_CONFIGS,
+];
