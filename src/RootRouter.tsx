d492c34ac56bcd56fbd104db2857b3a051e6cf14
Isaac Mtsiriza <230100836@luanar.ac.mw>
2026-03-24 01:33:57 +0200

    Add Batch B account routes and first profile/auth pages

--- src/RootRouter.tsx
+++ src/RootRouter.tsx
@@ -2,10 +2,14 @@ import { useEffect, useState } from "react";
 import App from "./App";
 import CreateListingPage from "./CreateListingPage";
 import EditListingPage from "./EditListingPage";
+import ForgotPasswordPage from "./ForgotPasswordPage";
 import HomePage from "./HomePage";
+import LoginPage from "./LoginPage";
+import ProfilePage from "./ProfilePage";
 import SavedPage from "./SavedPage";
 import SettingsPage from "./SettingsPage";
 import SellerProfilePage from "./SellerProfilePage";
+import SignupPage from "./SignupPage";
 import { getAppRouteFromLocation, type AppRoute } from "./lib/appNavigation";
 
 export default function RootRouter() {
@@ -47,6 +51,22 @@ export default function RootRouter() {
 
   if (route === "edit") {
     return <EditListingPage />;
+  }
+
+  if (route === "login") {
+    return <LoginPage />;
+  }
+
+  if (route === "signup") {
+    return <SignupPage />;
+  }
+
+  if (route === "forgot_password") {
+    return <ForgotPasswordPage />;
+  }
+
+  if (route === "profile") {
+    return <ProfilePage />;
   }
 
   return <HomePage />;
