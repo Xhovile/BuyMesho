aa7f5265147c6e730a380af9b41e08cd9c82e1cf
Isaac Mtsiriza <230100836@luanar.ac.mw>
2026-03-24 02:02:13 +0200

    Refactor routing logic in RootRouter component

--- src/RootRouter.tsx
+++ src/RootRouter.tsx
@@ -1,7 +1,11 @@
 import { useEffect, useState } from "react";
 import App from "./App";
+import BecomeSellerPage from "./BecomeSellerPage";
+import ChangePasswordPage from "./ChangePasswordPage";
 import CreateListingPage from "./CreateListingPage";
+import EditAccountPage from "./EditAccountPage";
 import EditListingPage from "./EditListingPage";
+import EditProfilePage from "./EditProfilePage";
 import ForgotPasswordPage from "./ForgotPasswordPage";
 import HomePage from "./HomePage";
 import LoginPage from "./LoginPage";
@@ -29,45 +33,20 @@ export default function RootRouter() {
     };
   }, []);
 
-  if (route === "explore") {
-    return <App />;
-  }
-
-  if (route === "saved") {
-    return <SavedPage />;
-  }
-
-  if (route === "settings") {
-    return <SettingsPage />;
-  }
-
-  if (route === "seller") {
-    return <SellerProfilePage />;
-  }
-
-  if (route === "create") {
-    return <CreateListingPage />;
-  }
-
-  if (route === "edit") {
-    return <EditListingPage />;
-  }
-
-  if (route === "login") {
-    return <LoginPage />;
-  }
-
-  if (route === "signup") {
-    return <SignupPage />;
-  }
-
-  if (route === "forgot_password") {
-    return <ForgotPasswordPage />;
-  }
-
-  if (route === "profile") {
-    return <ProfilePage />;
-  }
+  if (route === "explore") return <App />;
+  if (route === "saved") return <SavedPage />;
+  if (route === "settings") return <SettingsPage />;
+  if (route === "seller") return <SellerProfilePage />;
+  if (route === "create") return <CreateListingPage />;
+  if (route === "edit") return <EditListingPage />;
+  if (route === "login") return <LoginPage />;
+  if (route === "signup") return <SignupPage />;
+  if (route === "forgot_password") return <ForgotPasswordPage />;
+  if (route === "profile") return <ProfilePage />;
+  if (route === "edit_profile") return <EditProfilePage />;
+  if (route === "edit_account") return <EditAccountPage />;
+  if (route === "become_seller") return <BecomeSellerPage />;
+  if (route === "change_password") return <ChangePasswordPage />;
 
   return <HomePage />;
 }
