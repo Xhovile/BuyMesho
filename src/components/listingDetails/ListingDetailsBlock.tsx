ee654592091d7f2bc389f1bae0d2c315e6c5d3e6
Isaac Mtsiriza <230100836@luanar.ac.mw>
2026-04-26 11:15:26 +0200

    Update import path for SectionHeading component

--- src/components/listingDetails/ListingDetailsBlock.tsx
+++ src/components/listingDetails/ListingDetailsBlock.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from "react";
-import { SectionHeading } from "../../../listingDetails/ListingDetailsShared";
+import { SectionHeading } from "./listingDetails/ListingDetailsShared";
 
 export default function ListingDetailsBlock({
   description,
