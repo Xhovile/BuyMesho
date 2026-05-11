39349983e233ecb6ef4580c825799475c93d44ac
Isaac Mtsiriza <230100836@luanar.ac.mw>
2026-05-11 13:20:31 +0200

    Update header text and color for Buyer Analytics

--- src/BuyerPaymentsPage.tsx
+++ src/BuyerPaymentsPage.tsx
@@ -87,8 +87,8 @@ function BuyerPaymentsPageContent() {
           </button>
         </div>
 
-            <h1 className="text-4xl font-black tracking-tight text-zinc-950 sm:text-5xl">
-            Buyer Analytics
+            <h1 className="text-4xl font-black tracking-tight text-zinc-650 sm:text-5xl">
+            BUYER ANALYTICS
             </h1>
 
         <div className="mt-2 flex flex-wrap items-center gap-3">
