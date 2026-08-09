54c22c3d91e0f3c29146a0cdfb48a9209f1f0d1d
Isaac Mtsiriza <230100836@luanar.ac.mw>
2026-08-09 15:10:40 +0200

    Add route-local admin access compatibility module

---
+++ server/routes/adminAccess.ts
@@ -0,0 +1,7 @@
+export {
+  getConfiguredAdminEmails,
+  getConfiguredAdminUids,
+  isConfiguredAdmin,
+  hasAdminAccess,
+} from "../auth/adminAccess.js";
+export type { AdminIdentity } from "../auth/adminAccess.js";
