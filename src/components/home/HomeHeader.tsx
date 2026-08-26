9c0d99e19521d618f1a4603f64b17eccdb703b01
Isaac Mtsiriza <230100836@luanar.ac.mw>
2026-08-26 02:00:39 +0200

    Align home desktop hamburger with shared menu

--- src/components/home/HomeHeader.tsx
+++ src/components/home/HomeHeader.tsx
@@ -138,7 +138,6 @@ export default function HomeHeader({ controller }: { controller: HomePageControl
                   </button>
 
                   <HeaderDesktopMenu
-                    menuRef={desktopMenuRef}
                     open={desktopMenuOpen}
                     isLoggedIn={isLoggedIn}
                     isSeller={isSeller}
