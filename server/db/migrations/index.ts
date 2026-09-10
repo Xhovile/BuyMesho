f867acbbb8eda010e158bb4110ef973730196c30
Isaac Mtsiriza <230100836@luanar.ac.mw>
2026-09-10 10:22:36 +0200

    chore(disputes): run ownership migration with application migrations

--- server/db/migrations/index.ts
+++ server/db/migrations/index.ts
@@ -9,6 +9,7 @@ import { ensureRefundDisputeArchitectureMigration } from "./20260904_refund_disp
 import { ensureDisputeSupportRequestsMigration } from "./20260905_dispute_support_requests.js";
 import { ensureDisputeWindowsMigration } from "./20260905_dispute_windows.js";
 import { ensureDisputeTimestampCompatibilityMigration } from "./20260908_dispute_timestamp_compatibility.js";
+import { ensureDisputeResolutionOwnershipMigration } from "./20260910_dispute_resolution_ownership.js";
 
 function ensureExtraTables() {
   postgresDb.exec(`
@@ -155,5 +156,5 @@ function backfillOrderPaidAtFromPayments() { postgresDb.exec(`UPDATE orders SET 
 function backfillFulfilledAtFromUpdatedAt() { postgresDb.exec(`UPDATE orders SET fulfilled_at = updated_at WHERE status = 'fulfilled' AND fulfilled_at IS NULL AND updated_at IS NOT NULL AND updated_at >= COALESCE(paid_at, created_at);`); }
 
 export function runMigrations() {
-  ensureExtraTables(); ensureEventLifecycleSchema(); ensureEventOwnershipIntegrityMigration(); ensureMessageSchema(postgresDb); normalizeHardDeleteAfterColumn(); updateSellerPayoutAccountColumns(); ensurePayoutLifecycleSchema(); initPaymentSchema(postgresDb); ensureEventTicketStatsSchema(); ensureSellerOrdersIndexesMigration(); ensureRefundDisputeArchitectureMigration(); ensureDisputeSupportRequestsMigration(); ensureDisputeWindowsMigration(); ensureDisputeTimestampCompatibilityMigration(); backfillOrderPaidAtFromPayments(); backfillFulfilledAtFromUpdatedAt(); backfillEventTickets();
+  ensureExtraTables(); ensureEventLifecycleSchema(); ensureEventOwnershipIntegrityMigration(); ensureMessageSchema(postgresDb); normalizeHardDeleteAfterColumn(); updateSellerPayoutAccountColumns(); ensurePayoutLifecycleSchema(); initPaymentSchema(postgresDb); ensureEventTicketStatsSchema(); ensureSellerOrdersIndexesMigration(); ensureRefundDisputeArchitectureMigration(); ensureDisputeSupportRequestsMigration(); ensureDisputeWindowsMigration(); ensureDisputeTimestampCompatibilityMigration(); ensureDisputeResolutionOwnershipMigration(); backfillOrderPaidAtFromPayments(); backfillFulfilledAtFromUpdatedAt(); backfillEventTickets();
 }
